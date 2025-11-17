282c35243a498dbad2eca7fbf46221bc57c2716c
rahul jadon <rahuljadonhere@gmail.com>
2025-11-17 14:50:04 +0530

    Update .gitignore to include additional file patterns

---
+++ middleware.js
@@ -0,0 +1,65 @@
+const Listing = require("./models/listing");
+const Review = require("./models/review");
+const ExpressError = require("./utils/ExpressError.js");
+const { listingSchema, reviewSchema } = require("./schema.js");
+
+// LOGIN CHECK
+module.exports.isLoggedIn = (req, res, next) => {
+  console.log(req.path, "..", req.originalUrl);
+  if (!req.isAuthenticated()) {
+    req.session.redirectUrl = req.originalUrl;
+    req.flash("error", "You must be logged in!");
+    return res.redirect("/login");
+  }
+  next();
+};
+
+// REDIRECT SAVE
+module.exports.saveRedirectUrl = (req, res, next) => {
+  if (req.session.redirectUrl) {
+    res.locals.redirectUrl = req.session.redirectUrl;
+  }
+  next();
+};
+
+// CHECK OWNER
+module.exports.isOwner = async (req, res, next) => {
+  let { id } = req.params;
+  let listing = await Listing.findById(id);
+  if (!listing.owner._id.equals(res.locals.currUser._id)) {
+    req.flash("error", "You are not the owner!");
+    return res.redirect(`/listings/${id}`);
+  }
+  next();
+};
+
+// VALIDATE LISTING
+module.exports.validateListing = (req, res, next) => {
+  const { error } = listingSchema.validate(req.body, { abortEarly: false });
+  if (error) {
+    const errMsg = error.details.map(el => el.message).join(",");
+    throw new ExpressError(400, errMsg);
+  }
+  next();
+};
+
+// VALIDATE REVIEW
+module.exports.validateReview = (req, res, next) => {
+  const { error } = reviewSchema.validate(req.body);
+  if (error) {
+    const errMsg = error.details.map(el => el.message).join(",");
+    throw new ExpressError(404, errMsg);
+  }
+  next();
+};
+
+// CHECK REVIEW AUTHOR
+module.exports.isReviewAuthor = async (req, res, next) => {
+  let { id, reviewId } = req.params;
+  let review = await Review.findById(reviewId);
+  if (!review.author._id.equals(res.locals.currUser._id)) {
+    req.flash("error", "You are not the author!");
+    return res.redirect(`/listings/${id}`);
+  }
+  next();
+};
